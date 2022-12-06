70732e05c13b76e0539d2867cce3511ff0aa0eee
ashok450270 <119446086+ashok450270@users.noreply.github.com>
2022-12-06 12:35:27 +0530

    Add files via upload

---
+++ PasstiveOrNagative.java
@@ -0,0 +1,22 @@
+package assignment2;
+
+public class PasstiveOrNagative {
+    public static void main(String[] args) {
+		int number=35;
+		if(number>0)
+		{
+			
+		System.out.println("The num is pasitive");
+		}
+		else if(number<0)
+		{
+			System.out.println("The num is nagative");
+		}
+		
+		else 
+		{
+			System.out.println("The num is neither pasitive nor negative");
+		}
+	}
+
+}
